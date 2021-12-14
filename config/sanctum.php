369db25bbd714822eaf7005a6946f4290cce85af
dtc <dtc@server.com>
2021-12-14 22:54:23 +0300

    op

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'dev.np.net-performance.online,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'dev.np.net-performance.online,.net-performance.online,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
